ae86a07e6dcf838a3a0e196b0fd392e809fe25a4
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:41:08 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/moteur/Tour.java
@@ -0,0 +1,31 @@
+package moteur;
+
+
+/**
+* @generated
+*/
+public class Tour {
+    
+    /**
+    * @generated
+    */
+    private  numero;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public  getNumero() {
+        return this.numero;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setNumero( numero) {
+        this.numero = numero;
+    }
+    
+    
+}
