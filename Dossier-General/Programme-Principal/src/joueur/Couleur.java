4a44cbb62baf6d09cfcff283cda46f51352db73e
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:39:04 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/joueur/Couleur.java
@@ -0,0 +1,71 @@
+package joueur;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+* @generated
+*/
+public class Couleur {
+    
+    /**
+    * @generated
+    */
+    private Joueur joueur;
+    
+    /**
+    * @generated
+    */
+    private Set<CarteInfluence> carteInfluence;
+    
+    /**
+    * @generated
+    */
+    private  nom;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public CarteInfluence getCarteInfluence() {
+        return this.carteInfluence;
+    }
+    
+    /**
+    * @generated
+    */
+    public CarteInfluence setCarteInfluence(CarteInfluence carteInfluence) {
+        this.carteInfluence = carteInfluence;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getNom() {
+        return this.nom;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setNom( nom) {
+        this.nom = nom;
+    }
+    
+    
+}
