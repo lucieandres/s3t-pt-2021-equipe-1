4ad3dbcb7a4baa673c7e81d29b23ab9d49872fcb
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:40:04 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/moteur/Manche.java
@@ -0,0 +1,71 @@
+package moteur;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+* @generated
+*/
+public class Manche {
+    
+    /**
+    * @generated
+    */
+    private Set<Tour> tour;
+    
+    /**
+    * @generated
+    */
+    private  numero;
+    
+    /**
+    * @generated
+    */
+    private  estFinie;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Tour getTour() {
+        return this.tour;
+    }
+    
+    /**
+    * @generated
+    */
+    public Tour setTour(Tour tour) {
+        this.tour = tour;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getNumero() {
+        return this.numero;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setNumero( numero) {
+        this.numero = numero;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getEstFinie() {
+        return this.estFinie;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setEstFinie( estFinie) {
+        this.estFinie = estFinie;
+    }
+    
+    
+}
