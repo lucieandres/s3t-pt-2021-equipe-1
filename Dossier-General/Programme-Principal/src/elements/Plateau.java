3985fbccb2cfcf4e03c1f554f48728755f49c3f1
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:36:18 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/elements/Plateau.java
@@ -0,0 +1,109 @@
+package elements;
+
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+* @generated
+*/
+public class Plateau {
+    
+    /**
+    * @generated
+    */
+    private Set<ElementPlateau> elementPlateau;
+    
+    /**
+    * @generated
+    */
+    private Colonne colonne;
+    
+    /**
+    * @generated
+    */
+    private Reserve reserve;
+    
+    /**
+    * @generated
+    */
+    private CartesEnMain cartes_en_main;
+    
+    /**
+    * @generated
+    */
+    private Defausse défausse;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public ElementPlateau getElementPlateau() {
+        return this.elementPlateau;
+    }
+    
+    /**
+    * @generated
+    */
+    public ElementPlateau setElementPlateau(ElementPlateau elementPlateau) {
+        this.elementPlateau = elementPlateau;
+    }
+    
+    /**
+    * @generated
+    */
+    public Colonne getColonne() {
+        return this.colonne;
+    }
+    
+    /**
+    * @generated
+    */
+    public Colonne setColonne(Colonne colonne) {
+        this.colonne = colonne;
+    }
+    
+    /**
+    * @generated
+    */
+    public Reserve getReserve() {
+        return this.reserve;
+    }
+    
+    /**
+    * @generated
+    */
+    public Reserve setReserve(Reserve reserve) {
+        this.reserve = reserve;
+    }
+    
+    /**
+    * @generated
+    */
+    public CartesEnMain getCartes_en_main() {
+        return this.cartes_en_main;
+    }
+    
+    /**
+    * @generated
+    */
+    public CartesEnMain setCartes_en_main(CartesEnMain cartes_en_main) {
+        this.cartes_en_main = cartes_en_main;
+    }
+    
+    /**
+    * @generated
+    */
+    public Defausse getDéfausse() {
+        return this.défausse;
+    }
+    
+    /**
+    * @generated
+    */
+    public Defausse setDéfausse(Defausse défausse) {
+        this.défausse = défausse;
+    }
+    
+    
+}
