1fb4de85a4d5bf1148026afea951b09388be78e6
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:38:54 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/joueur/Bot.java
@@ -0,0 +1,31 @@
+package joueur;
+
+
+/**
+* @generated
+*/
+public class Bot extends Joueur {
+    
+    /**
+    * @generated
+    */
+    private  diificulte;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public  getDiificulte() {
+        return this.diificulte;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setDiificulte( diificulte) {
+        this.diificulte = diificulte;
+    }
+    
+    
+}
