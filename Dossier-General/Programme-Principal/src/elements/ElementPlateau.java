f05a7e8cd01478d476e2cc8adde2e64489041522
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:35:51 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/elements/ElementPlateau.java
@@ -0,0 +1,55 @@
+package elements;
+
+import java.util.Set;
+
+import joueur.Joueur;
+
+import java.util.HashSet;
+
+/**
+* @generated
+*/
+public class ElementPlateau {
+    
+    /**
+    * @generated
+    */
+    private Set<CarteInfluence> carteInfluence;
+    
+    /**
+    * @generated
+    */
+    private Joueur joueur;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public CarteInfluence getCarteInfluence() {
+        return this.carteInfluence;
+    }
+    
+    /**
+    * @generated
+    */
+    public CarteInfluence setCarteInfluence(CarteInfluence carteInfluence) {
+        this.carteInfluence = carteInfluence;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    
+}
