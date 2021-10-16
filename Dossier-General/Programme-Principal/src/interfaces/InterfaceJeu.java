1b04bd8baa5f07aa8c1640d7f80307edeb25c7df
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:38:07 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceJeu.java
@@ -0,0 +1,97 @@
+package interfaces;
+
+import joueur.Joueur;
+
+/**
+* @generated
+*/
+public class InterfaceJeu {
+    
+    /**
+    * @generated
+    */
+    private Systeme systeme;
+    
+    /**
+    * @generated
+    */
+    private Joueur joueur;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Systeme getSysteme() {
+        return this.systeme;
+    }
+    
+    /**
+    * @generated
+    */
+    public Systeme setSysteme(Systeme systeme) {
+        this.systeme = systeme;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public rejoindrePartie() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public jouer() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public placerCarte() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public defausserCarte() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public piocherCarte() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public renouvelerReserve() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public quitterJeu() {
+        //TODO
+    }
+    
+}
