a9d9c7160708c88656dc5d55f8e56312d2ea2f34
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:38:18 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceParametres.java
@@ -0,0 +1,91 @@
+package interfaces;
+
+import joueur.Joueur;
+
+/**
+* @generated
+*/
+public class InterfaceParametres {
+    
+    /**
+    * @generated
+    */
+    private Systeme systeme;
+    
+    /**
+    * @generated
+    */
+    private Joueur joueur;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Systeme getSysteme() {
+        return this.systeme;
+    }
+    
+    /**
+    * @generated
+    */
+    public Systeme setSysteme(Systeme systeme) {
+        this.systeme = systeme;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public activerSon() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public couperSon() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public activerMusique() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public couperMusique() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public changerLangue() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public changerTheme() {
+        //TODO
+    }
+    
+}
