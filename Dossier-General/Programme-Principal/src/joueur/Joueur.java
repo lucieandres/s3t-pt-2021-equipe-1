33f774f5207ea578dd7db768317f56ce1c709658
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:39:16 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/joueur/Joueur.java
@@ -0,0 +1,145 @@
+package joueur;
+
+
+/**
+* @generated
+*/
+public class Joueur {
+    
+    /**
+    * @generated
+    */
+    private Couleur couleur;
+    
+    /**
+    * @generated
+    */
+    private InterfaceJeu interfaceJeu;
+    
+    /**
+    * @generated
+    */
+    private InterfaceParametres interfaceParametres;
+    
+    /**
+    * @generated
+    */
+    private Resultat resultat;
+    
+    /**
+    * @generated
+    */
+    private InterfaceAttente interfaceAttente;
+    
+    /**
+    * @generated
+    */
+    private  pseudo;
+    
+    /**
+    * @generated
+    */
+    private  date_naissance;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Couleur getCouleur() {
+        return this.couleur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Couleur setCouleur(Couleur couleur) {
+        this.couleur = couleur;
+    }
+    
+    /**
+    * @generated
+    */
+    public InterfaceJeu getInterfaceJeu() {
+        return this.interfaceJeu;
+    }
+    
+    /**
+    * @generated
+    */
+    public InterfaceJeu setInterfaceJeu(InterfaceJeu interfaceJeu) {
+        this.interfaceJeu = interfaceJeu;
+    }
+    
+    /**
+    * @generated
+    */
+    public InterfaceParametres getInterfaceParametres() {
+        return this.interfaceParametres;
+    }
+    
+    /**
+    * @generated
+    */
+    public InterfaceParametres setInterfaceParametres(InterfaceParametres interfaceParametres) {
+        this.interfaceParametres = interfaceParametres;
+    }
+    
+    /**
+    * @generated
+    */
+    public Resultat getResultat() {
+        return this.resultat;
+    }
+    
+    /**
+    * @generated
+    */
+    public Resultat setResultat(Resultat resultat) {
+        this.resultat = resultat;
+    }
+    
+    /**
+    * @generated
+    */
+    public InterfaceAttente getInterfaceAttente() {
+        return this.interfaceAttente;
+    }
+    
+    /**
+    * @generated
+    */
+    public InterfaceAttente setInterfaceAttente(InterfaceAttente interfaceAttente) {
+        this.interfaceAttente = interfaceAttente;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getPseudo() {
+        return this.pseudo;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setPseudo( pseudo) {
+        this.pseudo = pseudo;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getDate_naissance() {
+        return this.date_naissance;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setDate_naissance( date_naissance) {
+        this.date_naissance = date_naissance;
+    }
+    
+    
+}
