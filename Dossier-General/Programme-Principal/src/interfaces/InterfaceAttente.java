eb5eee8e9f553be78734847bce8f1cf2e050079a
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:37:43 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceAttente.java
@@ -0,0 +1,61 @@
+package interfaces;
+
+import joueur.Joueur;
+
+/**
+* @generated
+*/
+public class InterfaceAttente {
+    
+    /**
+    * @generated
+    */
+    private Systeme systeme;
+    
+    /**
+    * @generated
+    */
+    private Joueur joueur;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Systeme getSysteme() {
+        return this.systeme;
+    }
+    
+    /**
+    * @generated
+    */
+    public Systeme setSysteme(Systeme systeme) {
+        this.systeme = systeme;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public estPret() {
+        //TODO
+    }
+    
+}
