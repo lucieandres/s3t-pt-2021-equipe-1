ec000027954ec5095c3675be85a5d95cb4020483
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:40:38 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/moteur/Resultat.java
@@ -0,0 +1,51 @@
+package moteur;
+
+import joueur.Joueur;
+
+/**
+* @generated
+*/
+public class Resultat {
+    
+    /**
+    * @generated
+    */
+    private Joueur joueur;
+    
+    /**
+    * @generated
+    */
+    private  valeur;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getValeur() {
+        return this.valeur;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setValeur( valeur) {
+        this.valeur = valeur;
+    }
+    
+    
+}
