95a7e462f656d9349e153ed42e09a5fc4df842a7
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:34:13 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/cartes/CarteObjectif.java
@@ -0,0 +1,69 @@
+package cartes;
+
+
+/**
+* @generated
+*/
+public class CarteObjectif {
+    
+    /**
+    * @generated
+    */
+    private  domaine;
+    
+    /**
+    * @generated
+    */
+    private  valeur;
+    
+    /**
+    * @generated
+    */
+    private  estRealise;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public  getDomaine() {
+        return this.domaine;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setDomaine( domaine) {
+        this.domaine = domaine;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getValeur() {
+        return this.valeur;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setValeur( valeur) {
+        this.valeur = valeur;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getEstRealise() {
+        return this.estRealise;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setEstRealise( estRealise) {
+        this.estRealise = estRealise;
+    }
+    
+    
+}
