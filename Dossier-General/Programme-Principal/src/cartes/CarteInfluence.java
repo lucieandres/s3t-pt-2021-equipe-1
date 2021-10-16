ea224f7673e84aa30565de5f65194ba1ba744e45
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:33:48 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/cartes/CarteInfluence.java
@@ -0,0 +1,88 @@
+package cartes;
+
+
+/**
+* @generated
+*/
+public class CarteInfluence {
+    
+    /**
+    * @generated
+    */
+    private  nom;
+    
+    /**
+    * @generated
+    */
+    private  capacite_speciale;
+    
+    /**
+    * @generated
+    */
+    private  valeur;
+    
+    /**
+    * @generated
+    */
+    private  visible;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public  getNom() {
+        return this.nom;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setNom( nom) {
+        this.nom = nom;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getCapacite_speciale() {
+        return this.capacite_speciale;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setCapacite_speciale( capacite_speciale) {
+        this.capacite_speciale = capacite_speciale;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getValeur() {
+        return this.valeur;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setValeur( valeur) {
+        this.valeur = valeur;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getVisible() {
+        return this.visible;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setVisible( visible) {
+        this.visible = visible;
+    }
+    
+    
+}
