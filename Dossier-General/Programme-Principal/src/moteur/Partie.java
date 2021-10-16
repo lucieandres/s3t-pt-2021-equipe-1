282d4e481c8c6e4b5af71e00d9315de2320cc3a7
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:40:27 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/moteur/Partie.java
@@ -0,0 +1,74 @@
+package moteur;
+
+import java.util.Set;
+
+import joueur.Joueur;
+
+import java.util.HashSet;
+
+/**
+* @generated
+*/
+public class Partie {
+    
+    /**
+    * @generated
+    */
+    private Set<Joueur> joueur;
+    
+    /**
+    * @generated
+    */
+    private Set<Manche> manche;
+    
+    /**
+    * @generated
+    */
+    private  code;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Joueur setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    /**
+    * @generated
+    */
+    public Manche getManche() {
+        return this.manche;
+    }
+    
+    /**
+    * @generated
+    */
+    public Manche setManche(Manche manche) {
+        this.manche = manche;
+    }
+    
+    /**
+    * @generated
+    */
+    public  getCode() {
+        return this.code;
+    }
+    
+    /**
+    * @generated
+    */
+    public  setCode( code) {
+        this.code = code;
+    }
+    
+    
+}
