a6dd70c775375d20ba0446ebcc84d0318728a9cd
neuville-burguiere benjamin <benjamin.neuville-burguiere@etu.univ-cotedazur.fr>
2021-10-16 09:35:15 +0000

    Upload New File

---
+++ Dossier-General/Programme-Principal/src/elements/Colonne.java
@@ -0,0 +1,31 @@
+package elements;
+
+
+/**
+* @generated
+*/
+public class Colonne extends ElementPlateau {
+    
+    /**
+    * @generated
+    */
+    private CarteObjectif carteObjectif;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public CarteObjectif getCarteObjectif() {
+        return this.carteObjectif;
+    }
+    
+    /**
+    * @generated
+    */
+    public CarteObjectif setCarteObjectif(CarteObjectif carteObjectif) {
+        this.carteObjectif = carteObjectif;
+    }
+    
+    
+}
