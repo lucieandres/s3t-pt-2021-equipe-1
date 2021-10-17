3ab3d05488e124f6eac28a80f256671996eac615
lucieandres <lucie.andres@etu.unice.fr>
2021-10-17 17:52:02 +0200

    classe InterfaceMenu
    création d'une nouvelle classe pour mieux séparer les interfaces

---
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceMenu.java
@@ -0,0 +1,98 @@
+package interfaces;
+
+import joueur.Joueur;
+import moteur.Systeme;
+
+/**
+* @generated
+*/
+public class InterfaceMenu {
+    
+    /**
+    * attribut systeme de la classe Systeme, package moteur
+    */
+    private Systeme systeme;
+    
+    /**
+    * attribut joueur de la classe Joueur, package joueur
+    */
+    private Joueur joueur;
+    
+    
+    
+    /**
+    * affiche systeme
+    */
+    public Systeme getSysteme() {
+        return this.systeme;
+    }
+    
+    /**
+    * modifie systeme
+    */
+    public void setSysteme(Systeme systeme) {
+        this.systeme = systeme;
+    }
+    
+    /**
+    * affiche joueur
+    */
+    public Joueur getJoueur() {
+        return this.joueur;
+    }
+    
+    /**
+    * modifie joueur
+    */
+    public void setJoueur(Joueur joueur) {
+        this.joueur = joueur;
+    }
+    
+    
+
+    //                          Operations                                  
+    
+    /**
+    * @generated
+    */
+    public rejoindrePartie() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public jouer() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public placerCarte() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public defausserCarte() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public piocherCarte() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public renouvelerReserve() {
+        //TODO
+    }
+    /**
+    * @generated
+    */
+    public quitterJeu() {
+        //TODO
+    }
+    
+}
