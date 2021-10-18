58f904affbcab244768b9e889f8021a775ae1c97
Thi Thanh Tu DUONG <thi-thanh-tu.duong@etu.univ-cotedazur.fr>
2021-10-18 16:20:43 +0200

    Update Bot.java
    fix eurreur+constructeur

--- Dossier-General/Programme-Principal/src/joueur/Bot.java
+++ Dossier-General/Programme-Principal/src/joueur/Bot.java
@@ -1,32 +1,40 @@
 package joueur;
 
+import interfaces.InterfaceAttente;
+import interfaces.InterfaceJeu;
+import interfaces.InterfaceParametres;
+import moteur.Resultat;
 
 /**
 * @generated
 */
 public class Bot extends Joueur {
-	//test
-    
+
     /**
     * @generated
     */
-    private  diificulte;
+    private int difficulte;
+
     
-    
-    
+	public Bot(int difficulte,Couleur couleur, InterfaceJeu interfaceJeu, InterfaceParametres interfaceParametres,
+			Resultat resultat, InterfaceAttente interfaceAttente, String pseudo) {
+		super(couleur, interfaceJeu, interfaceParametres, resultat, interfaceAttente, pseudo);
+		this.difficulte = difficulte;
+	}
+	public int getDifficulte() {
+		return difficulte;
+	}
+	/**
+	* @generated
+	*/
+	public void setDifficulte(int difficulte) {
+		this.difficulte = difficulte;
+	}
+
     /**
     * @generated
     */
-    public  getDiificulte() {
-        return this.diificulte;
-    }
     
-    /**
-    * @generated
-    */
-    public  setDiificulte( diificulte) {
-        this.diificulte = diificulte;
-    }
-    
-    
+
+
 }
