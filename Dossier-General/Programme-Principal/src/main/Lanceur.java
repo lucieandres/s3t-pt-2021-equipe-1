af33ea7a2bd4127192f866e6019fc4ac5001815c
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2021-10-19 17:55:11 +0200

    Début

---
+++ Dossier-General/Programme-Principal/src/main/Lanceur.java
@@ -0,0 +1,14 @@
+package main;
+ 
+import interfaces.InterfaceMenu;
+
+public class Lanceur{ 
+  
+	public static void main(String[] args) {
+		//Voir projet de TD d'IHM
+		//Ici on devra lancer lancer le processus (Créer une var sys jsp si vous avez créé une classe "Système")
+		InterfaceMenu.lancement(args, [entrer la var sys].lancer());
+	}
+}
+	
+
