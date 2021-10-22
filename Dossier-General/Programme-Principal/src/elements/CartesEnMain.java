d2e0a402bbe8042036a630953046ba5610baf04f
MichelGulasin-Triay <70748262+MichelGulasin-Triay@users.noreply.github.com>
2021-10-22 17:09:21 +0200

    Update CartesEnMain.java

--- Dossier-General/Programme-Principal/src/elements/CartesEnMain.java
+++ Dossier-General/Programme-Principal/src/elements/CartesEnMain.java
@@ -1,13 +1,34 @@
 package elements;
 
+import java.util.ArrayList;
+
+import cartes.CarteInfluence;
+
 /**
 * @generated
 */
 public class CartesEnMain extends ElementPlateau {
     
-        //constructeur
-    public CartesEnMain() {
-    	//TODO
+    private ArrayList<CarteInfluence> cartesInfluences = new ArrayList<>();
+        
+    //constructeur
+    public CartesEnMain(ArrayList<CarteInfluence> cartesInfluences) {
+    	this.cartesInfluences = cartesInfluences;
     }    
     
+    public ArrayList<CarteInfluence> getCartesInfluences(){
+        return this.cartesInfluences;
+    }
+
+    public void setCarteInfluences(ArrayList<CarteInfluence> cartesInfluences){
+        this.cartesInfluences = cartesInfluences;
+    }
+
+    public void prendreCarte(CarteInfluence carteInfluence){
+        cartesInfluences.add(carteInfluence);
+    }
+
+    public void poserCarte(CarteInfluence carteInfluence){
+        cartesInfluences.remove(carteInfluence);
+    }
 }
