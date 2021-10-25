cd3ddd26bd22179de2ed90024fd73997464c0ce5
lucieandres <lucie.andres@etu.unice.fr>
2021-10-25 16:02:17 +0200

    Update TestsCarteInfluence.java

--- Dossier-General/Programme-Principal/src/tests/TestsCarteInfluence.java
+++ Dossier-General/Programme-Principal/src/tests/TestsCarteInfluence.java
@@ -1,12 +1,14 @@
 package tests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TestsCarteInfluence {
+import cartes.CarteInfluence;
+
+class Tests {
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -18,7 +20,21 @@ class TestsCarteInfluence {
 
 	@Test
 	void test() {
-		fail("Not yet implemented");
+		
+		CarteInfluence carteInfluenceRoi = new CarteInfluence("Ro", "La valeur de l'Ermite diminue de 1 pour chaque autre carte présente dans la colonne", 2, true);
+		assertEquals(carteInfluenceRoi.getNom(), "Ro");
+		assertEquals(carteInfluenceRoi.getCapaciteSpeciale(), "La valeur de l'Ermite diminue de 1 pour chaque autre carte présente dans la colonne");
+		assertEquals(carteInfluenceRoi.getValeur(), 2);
+		assertEquals(carteInfluenceRoi.getVisible(), true);
+		carteInfluenceRoi.setNom("Roi");
+		carteInfluenceRoi.setCapaciteSpeciale("null");
+		carteInfluenceRoi.setValeur(20);
+		carteInfluenceRoi.setVisible(false);
+		assertEquals(carteInfluenceRoi.getNom(), "Roi");
+		assertEquals(carteInfluenceRoi.getCapaciteSpeciale(), "null");
+		assertEquals(carteInfluenceRoi.getValeur(), 20);
+		assertEquals(carteInfluenceRoi.getVisible(), false);
+		
 	}
 
 }
