e5e87272e61a86ad736461b3cb16facb8c0af624
Benjamin Neuville <b.neuville@outlook.fr>
2021-11-05 16:04:24 +0100

    plateau

--- Dossier-General/Programme-Principal/src/elements/Plateau.java
+++ Dossier-General/Programme-Principal/src/elements/Plateau.java
@@ -9,51 +9,17 @@ import java.util.Set;
 */
 public class Plateau {
     
-
-    /**
-    * @generated
-    */
-    private ArrayList<ElementPlateau> elementPlateau;
-    
     /**
     * @generated
     */
     private ArrayList<Colonne> colonne;
     
-    /**
-    * @generated
-    */
-    private Reserve reserve;
-    
-    /**
-    * @generated
-    */
-    private CartesEnMain cartesEnMain;
-    
-    /**
-    * @generated
-    */
-    private Defausse defausse;
     
 	//constructeur
-	public Plateau(ArrayList<ElementPlateau> elementPlateau) {
-		this.elementPlateau=elementPlateau;
+	public Plateau(ArrayList<Colonne> colonne) {
+		this.colonne=colonne;
 	}
-    
-    /**
-    * @generated
-    */
-    public ArrayList<ElementPlateau> getElementPlateau() {
-        return this.elementPlateau;
-    }
-    
-    /**
-    * @generated
-    */
-    public void setElementPlateau(ElementPlateau elementPlateau) {
-        this.elementPlateau.add(elementPlateau);
-    }
-    
+	
     /**
     * @generated
     */
@@ -68,47 +34,6 @@ public class Plateau {
         this.colonne = colonne;
     }
     
-    /**
-    * @generated
-    */
-    public Reserve getReserve() {
-        return this.reserve;
-    }
-    
-    /**
-    * @generated
-    */
-    public Reserve setReserve(Reserve reserve) {
-        return this.reserve = reserve;
-    }
-    
-    /**
-    * @generated
-    */
-    public CartesEnMain getCartesEnMain() {
-        return this.cartesEnMain;
-    }
-    
-    /**
-    * @generated
-    */
-    public void setCartesEnMain(CartesEnMain cartesEnMain) {
-        this.cartesEnMain = cartesEnMain;
-    }
-    
-    /**
-    * @generated
-    */
-    public Defausse getDefausse() {
-        return this.defausse;
-    }
-    
-    /**
-    * @generated
-    */
-    public void setDefausse(Defausse defausse) {
-        this.defausse = defausse;
-    }
-    
+    //Methodes
     
 }
