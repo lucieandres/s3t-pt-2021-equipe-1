2bb6a0f5ded6752e4e777fa1e48382f86813d1db
MichelGulasin-Triay <70748262+MichelGulasin-Triay@users.noreply.github.com>
2021-11-05 15:42:33 +0100

    Update Reserve.java

--- Dossier-General/Programme-Principal/src/elements/Reserve.java
+++ Dossier-General/Programme-Principal/src/elements/Reserve.java
@@ -11,10 +11,18 @@ public class Reserve extends ElementPlateau {
     
         private static ArrayList<CarteInfluence> cartesInfluences;
         private static Joueur joueur;
+        private Defausse defausse;
 
     //constructeur
     public Reserve() {
     	super(cartesInfluences, joueur);
+    }
+
+    //remplir la reserve
+    public void reserveVide(){
+        if(cartesInfluences.size()==0){
+            cartesInfluences.addAll(defausse.getCartesInfluences());
+        }
     }
     
 }
