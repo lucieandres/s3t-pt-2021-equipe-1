bb84651df4c9b6a2f976436542ba2ee10096ff73
lucieandres <lucie.andres@etu.unice.fr>
2021-11-09 08:58:10 +0100

    Update TestsCarteInfluence.java

--- Dossier-General/Programme-Principal/src/tests/TestsCarteInfluence.java
+++ Dossier-General/Programme-Principal/src/tests/TestsCarteInfluence.java
@@ -6,7 +6,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import cartes.Alchimiste;
 import cartes.CarteInfluence;
+import cartes.Juliette;
+import cartes.Reine;
 import cartes.Roi;
 import joueur.Couleur;
 
@@ -24,17 +27,34 @@ class Tests {
 	void test() {
 		
 		Couleur blanc = new Couleur("blanc");
+		Couleur bleu = new Couleur("bleu");
+		
+		Juliette julBl = new Juliette(blanc);
+		assertEquals(julBl.getNom(), "Juliette");
+		assertEquals(julBl.getCouleur(), blanc);
+		assertEquals(julBl.getEstVisible(), false);
+		julBl.setCouleur(bleu);
+		julBl.setNom("Reine");
+		julBl.setValeur(10);
+		julBl.setEstVisible(true);
+
+		Reine reiBl = new Reine(blanc);
+		assertEquals(reiBl.getNom(), "Reine");
+		assertEquals(reiBl.getCouleur(), blanc);
+		assertEquals(reiBl.getEstVisible(), false);
+		reiBl.setCouleur(bleu);
+		reiBl.setNom("Roi");
+		reiBl.setValeur(10);
+		reiBl.setEstVisible(true);
+		
 		Roi roiBl = new Roi(blanc);
 		assertEquals(roiBl.getNom(), "Roi");
 		assertEquals(roiBl.getCouleur(), blanc);
-		assertEquals(roiBl.getValeur(), 20);
 		assertEquals(roiBl.getEstVisible(), false);
-		Couleur bleu = new Couleur("bleu");
 		roiBl.setCouleur(bleu);
 		roiBl.setNom("Reine");
 		roiBl.setValeur(10);
 		roiBl.setEstVisible(true);
-
 		
 	}
 
