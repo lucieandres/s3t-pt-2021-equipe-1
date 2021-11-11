5a808d9f443fd6496a90f29439820fd5edf1bc35
ChristopheDjendli <90375010+ChristopheDjendli@users.noreply.github.com>
2021-11-11 13:15:14 +0100

    Reserve / Defausse

--- Dossier-General/Programme-Principal/src/elements/Defausse.java
+++ Dossier-General/Programme-Principal/src/elements/Defausse.java
@@ -1,6 +1,7 @@
 package elements;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 import cartes.CarteInfluence;
 
@@ -9,20 +10,29 @@ import cartes.CarteInfluence;
 */
 public class Defausse extends ElementPlateau {
     
-	ArrayList<CarteInfluence> cartesInfluences;
+	private ArrayList<CarteInfluence> cartesInfluences;
 	
         //constructeur
     public Defausse() {
-    	
+    	this.cartesInfluences = null;
     }
     
-    public void addDefausseCartesInfluences(CarteInfluence carteInfluence) {
+    public void addDefausseCarteInfluence(CarteInfluence carteInfluence) {
+    	if(!carteInfluence.getEstVisible())
+    		carteInfluence.setEstVisible(true);
+    	
     	this.cartesInfluences.add(carteInfluence);
     }
     
-    public void sortDefausse() { //si la reserve du joueur est epuisee, il melange sa defausse et la reutilise tant que reserve face cachee
-    	
+    public void shuffleDefausse() { //si la reserve du joueur est epuisee, il melange sa défausse et la réutilise en tant que reserve face cachée
+    	Collections.shuffle(cartesInfluences);
     }
     
+    public void viderDefausse() { //si la réserve a été reconstituée par les cartes de la défausse, celle-ci doit être vidée
+    	this.cartesInfluences.clear();
+    }
     
+    public ArrayList<CarteInfluence> getDefausse(){
+    	return this.cartesInfluences;
+    }
 }
--- Dossier-General/Programme-Principal/src/elements/Reserve.java
+++ Dossier-General/Programme-Principal/src/elements/Reserve.java
@@ -1,34 +1,40 @@
-//package elements;
-//
-//import java.util.ArrayList;
-//import cartes.CarteInfluence;
-//
-///**
-//* @generated
-//*/
-//public class Reserve {
-//    
-//	private ArrayList<CarteInfluence> cartesInfluences;
-//    private Defausse defausse;
-//        
-//    //constructeur
-//    public Reserve(ArrayList<CarteInfluence> cartesInfluences) {
-//       	this.cartesInfluences = cartesInfluences;
-//    }    
-//
-//    public ArrayList<CarteInfluence> getCartesInfluences() {
-//    	return this.cartesInfluences;
-//    }
-//        
-//    public void setCartesInfluences(ArrayList<CarteInfluence> cartesInfluences) {
-//    	this.cartesInfluences = cartesInfluences;
-//    }
-//
-//    //remplir la reserve
-//    public void reserveVide(){
-//    	if(cartesInfluences.size()==0){
-//    		cartesInfluences.addAll(defausse.getCartesInfluences());
-//        }
-//    }
-//    
-//}
+package elements;
+
+import java.util.ArrayList;
+import cartes.CarteInfluence;
+
+/**
+* @generated
+*/
+public class Reserve {
+    
+	private ArrayList<CarteInfluence> cartesInfluences;
+    private Defausse defausse;
+        
+    //constructeur
+    public Reserve(ArrayList<CarteInfluence> cartesInfluences) {
+       	this.cartesInfluences = cartesInfluences;
+    }    
+
+    public ArrayList<CarteInfluence> getCartesInfluences() {
+    	return this.cartesInfluences;
+    }
+        
+    public void setCartesInfluences(ArrayList<CarteInfluence> cartesInfluences) {
+    	this.cartesInfluences = cartesInfluences;
+    }
+
+    //remplir la reserve si elle a été vidée
+    public void remplirReserve(){
+    	if(cartesInfluences.size()==0){
+    		defausse.shuffleDefausse();
+    		for(CarteInfluence carteInfluence : defausse.getDefausse()) {
+        		carteInfluence.setEstVisible(false);
+        		this.cartesInfluences.add(carteInfluence);
+        	}
+    		cartesInfluences.addAll(defausse.getCartesInfluences());
+    		defausse.viderDefausse();
+        }
+    }
+    
+}
