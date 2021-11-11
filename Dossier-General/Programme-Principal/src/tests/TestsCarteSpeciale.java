b03bce1ad5f3cfac480a203e85e11dc83fddee41
lucieandres <lucie.andres@etu.unice.fr>
2021-11-11 12:17:56 +0100

    Update TestsCarteSpeciale.java

--- Dossier-General/Programme-Principal/src/tests/TestsCarteSpeciale.java
+++ Dossier-General/Programme-Principal/src/tests/TestsCarteSpeciale.java
@@ -1,24 +1,24 @@
-package tests;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-class TestsCarteSpeciale {
-
-	@BeforeEach
-	void setUp() throws Exception {
-	}
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//package tests;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//
+//class TestsCarteSpeciale {
+//
+//	@BeforeEach
+//	void setUp() throws Exception {
+//	}
+//
+//	@AfterEach
+//	void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
