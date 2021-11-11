b2586efb24d6f266b364e45c89725e4e5ff66159
lucieandres <lucie.andres@etu.unice.fr>
2021-11-11 12:13:06 +0100

    Update TestsSysteme.java

--- Dossier-General/Programme-Principal/src/tests/TestsSysteme.java
+++ Dossier-General/Programme-Principal/src/tests/TestsSysteme.java
@@ -1,21 +1,21 @@
-package tests;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-
-import moteur.Systeme;
-
-class TestsSysteme {
-
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
-
-}
+//package tests;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//
+//import moteur.Systeme;
+//
+//class TestsSysteme {
+//
+//	@AfterEach
+//	void tearDown() throws Exception {
+//	}
+//
+//	@Test
+//	void test() {
+//		fail("Not yet implemented");
+//	}
+//
+//}
