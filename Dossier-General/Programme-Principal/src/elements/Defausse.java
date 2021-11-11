81ff3093356bdd70764832d8d62e4eb00818668b
Benjamin Neuville <b.neuville@outlook.fr>
2021-11-11 12:20:41 +0100

    defausse out

--- Dossier-General/Programme-Principal/src/elements/Defausse.java
+++ Dossier-General/Programme-Principal/src/elements/Defausse.java
@@ -1,28 +1,28 @@
-package elements;
-
-import java.util.ArrayList;
-
-import cartes.CarteInfluence;
-
-/**
-* @generated
-*/
-public class Defausse extends ElementPlateau {
-    
-	ArrayList<CarteInfluence> cartesInfluences;
-	
-        //constructeur
-    public Defausse() {
-    	
-    }
-    
-    public void addDefausseCartesInfluences(CarteInfluence carteInfluence) {
-    	this.cartesInfluences.add(carteInfluence);
-    }
-    
-    public void sortDefausse() { //si la reserve du joueur est epuisee, il melange sa defausse et la reutilise tant que reserve face cachee
-    	
-    }
-    
-    
-}
+//package elements;
+//
+//import java.util.ArrayList;
+//
+//import cartes.CarteInfluence;
+//
+///**
+//* @generated
+//*/
+//public class Defausse extends ElementPlateau {
+//    
+//	ArrayList<CarteInfluence> cartesInfluences;
+//	
+//        //constructeur
+//    public Defausse() {
+//    	
+//    }
+//    
+//    public void addDefausseCartesInfluences(CarteInfluence carteInfluence) {
+//    	this.cartesInfluences.add(carteInfluence);
+//    }
+//    
+//    public void sortDefausse() { //si la reserve du joueur est epuisee, il melange sa defausse et la reutilise tant que reserve face cachee
+//    	
+//    }
+//    
+//    
+//}
