bbf9323fa46512a3a5e998482c299a37a1f68535
Benjamin Neuville <b.neuville@outlook.fr>
2021-11-11 20:43:11 +0100

    Update Bot.java

--- Dossier-General/Programme-Principal/src/bot/Bot.java
+++ Dossier-General/Programme-Principal/src/bot/Bot.java
@@ -1,23 +1,29 @@
 package bot;
 
+import java.util.ArrayList;
+
 import interfaces.InterfaceAttente;
 import interfaces.InterfaceJeu;
 import interfaces.InterfaceParametres;
 import javafx.scene.paint.Color;
+import joueur.Joueur;
 
 
-public class Bot{
+public class Bot extends Joueur {
 
 	private Color couleur;
 	private String pseudo;
     private String difficulte;
 	ArrayList<String> typeDifficulte = new ArrayList<String>();
-	typeDifficulte.add("facile","moyen","difficile");
 
 	public Bot(String difficulte, Color couleur, String pseudo) {
+		super(couleur, pseudo);
+		typeDifficulte.add("facile");
+		typeDifficulte.add("moyen");
+		typeDifficulte.add("difficile");
 		this.couleur=couleur;
 		this.pseudo=pseudo;
-		if !(typeDifficulte.contains(difficulte)) { throw new IllegalArgumentException("Type de difficulte n'existe pas"); }
+		if (!typeDifficulte.contains(difficulte)) { throw new IllegalArgumentException("Type de difficulte n'existe pas"); }
 		this.difficulte = difficulte;
 	}
 	/*
