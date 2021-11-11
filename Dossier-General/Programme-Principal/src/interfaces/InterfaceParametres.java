6101c425379cc36ddcd6a2238ac95668cd7e7b3e
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2021-11-11 13:53:32 +0100

    MAJ IMPORT

--- Dossier-General/Programme-Principal/src/interfaces/InterfaceParametres.java
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceParametres.java
@@ -6,11 +6,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
-import joueur.Joueur;
-import joueur.Joueur;
 
 /**
 * @generated
