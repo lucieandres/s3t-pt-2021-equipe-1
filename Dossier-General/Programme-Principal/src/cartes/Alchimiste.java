8e056cce753b96aaee1f5efce235e16e0258e503
lucieandres <lucie.andres@etu.unice.fr>
2021-11-11 18:22:46 +0100

    mise en commentaires des cartes non utilisées pour l'étape 2

--- Dossier-General/Programme-Principal/src/cartes/Alchimiste.java
+++ Dossier-General/Programme-Principal/src/cartes/Alchimiste.java
@@ -1,11 +1,11 @@
-package cartes;
-
-import javafx.scene.paint.Color;
-
-public class Alchimiste extends CarteDouble {
-	
-	public Alchimiste(Color couleur) {
-		super(couleur, "Alchimiste", "Alchimie");
-	}
-
-}
+//package cartes;
+//
+//import javafx.scene.paint.Color;
+//
+//public class Alchimiste extends CarteDouble {
+//	
+//	public Alchimiste(Color couleur) {
+//		super(couleur, "Alchimiste", "Alchimie");
+//	}
+//
+//}
--- Dossier-General/Programme-Principal/src/cartes/Traitre.java
+++ Dossier-General/Programme-Principal/src/cartes/Traitre.java
@@ -1,40 +1,40 @@
-package cartes;
-
-import javafx.scene.paint.Color;
-
-public class Traitre extends CarteSpeciale{
-	
-	public Traitre(Color couleur) {
-		super(couleur, "Le Traitre", 10);
-	}
-
-
-	@Override
-	public void Activer() {
-		CarteObjectif carteAEchanger = new CarteObjectif("Alchimie", 5); //carte random pour le moment
-		//TODO il faut trouver un moyen de choisir la carte
-		
-		int nbObjectif = this.getColonne().getPlateau().getColonne().size();
-		int numeroColonneVisee = 0;
-				
-		for(int i=0; i<nbObjectif; i++){
-			if(this.getColonne().getPlateau().getColonne().get(i).getCarteObjectif() == carteAEchanger){
-				numeroColonneVisee = i;
-				break;
-				}
-			//TODO erreur si il trouve pas
-		}
-		
-		this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).setCarteObjectif(this.getColonne().getCarteObjectif());
-		if (this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCarteObjectif().getValeur()==this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCartesInfluences().size()) {
-			this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCarteObjectif().Realiser();
-		}
-		
-		this.getColonne().setCarteObjectif(carteAEchanger);
-		if (this.getColonne().getCarteObjectif().getValeur()==this.getColonne().getCartesInfluences().size()) {
-			this.getColonne().getCarteObjectif().Realiser();
-		}
-		
-	}
-
-}
+//package cartes;
+//
+//import javafx.scene.paint.Color;
+//
+//public class Traitre extends CarteSpeciale{
+//	
+//	public Traitre(Color couleur) {
+//		super(couleur, "Le Traitre", 10);
+//	}
+//
+//
+//	@Override
+//	public void Activer() {
+//		CarteObjectif carteAEchanger = new CarteObjectif("Alchimie", 5); //carte random pour le moment
+//		//TODO il faut trouver un moyen de choisir la carte
+//		
+//		int nbObjectif = this.getColonne().getPlateau().getColonne().size();
+//		int numeroColonneVisee = 0;
+//				
+//		for(int i=0; i<nbObjectif; i++){
+//			if(this.getColonne().getPlateau().getColonne().get(i).getCarteObjectif() == carteAEchanger){
+//				numeroColonneVisee = i;
+//				break;
+//				}
+//			//TODO erreur si il trouve pas
+//		}
+//		
+//		this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).setCarteObjectif(this.getColonne().getCarteObjectif());
+//		if (this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCarteObjectif().getValeur()==this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCartesInfluences().size()) {
+//			this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCarteObjectif().Realiser();
+//		}
+//		
+//		this.getColonne().setCarteObjectif(carteAEchanger);
+//		if (this.getColonne().getCarteObjectif().getValeur()==this.getColonne().getCartesInfluences().size()) {
+//			this.getColonne().getCarteObjectif().Realiser();
+//		}
+//		
+//	}
+//
+//}
