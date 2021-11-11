da74d085a15fd3f7be462daa4291ef9fe74f70aa
Benjamin Neuville <b.neuville@outlook.fr>
2021-11-12 00:38:30 +0100

    FAIRE JOUER LA CARTE LEZGO

--- Dossier-General/Programme-Principal/src/elements/Colonne.java
+++ Dossier-General/Programme-Principal/src/elements/Colonne.java
@@ -4,112 +4,61 @@ import cartes.CarteInfluence;
 import cartes.CarteObjectif;
 
 /**
- * Cette classe définit les colonnes du plateau de jeu qui vont chacune contenir une carte <i>Objectif</i> et des
- * cartes <i>Influence</i> qui vont changer au cours de la partie.
- * 
- * @author S3T - G1
- * 
- * @since 1.0
- */
+* @generated
+*/
 public class Colonne {
 
     private CarteInfluence[] cartesInfluences;
     private CarteObjectif carteObjectif;
 
-    /**
-     * Ce constructeur produit une colonne avec un emplacement pour une carte <i>Objectif</i> et plusieurs emplacements
-     * pour les cartes <i>Influence</i>, le nombre d'emplacements est défini à partir du nombre de joueurs spécifié.
-     * 
-     * @param nbjoueur Le nombre de joueurs.
-     * 
-     * @since 1.0
-     */
+    //constructeur
 	public Colonne(int nbjoueurs) {
         this.carteObjectif = null;
         this.cartesInfluences = new CarteInfluence[6+(nbjoueurs)*2];
 	}
 
-	/**
-     * Retourne les cartes <i>Influence</i> de la colonne.
-     * 
-     * @return Les cartes <i>Influence</i> de la colonne.
-     * 
-     * @since 1.0
-     */
+
 	public CarteInfluence[] getCartesInfluences() {
         return this.cartesInfluences;
     }
 
-	/**
-     * Modifie les cartes <i>Influence</i> de la colonne avec celles spécifiées.
-     * 
-     * @param cartesInfluences Les cartes <i>Influence</i> à assigner à la colonne.
-     * 
-     * @since 1.0
-     */
     public void setCartesInfluences(CarteInfluence[] cartesInfluences) {
         this.cartesInfluences = cartesInfluences;
     }
 
     /**
-     * Retourne la carte <i>Objectif</i> de la colonne.
-     * 
-     * @return La carte <i>Objectif</i> de la colonne.
-     * 
-     * @since 1.0
-     */
+    * @generated
+    */
     public CarteObjectif getCarteObjectif() {
         return this.carteObjectif;
     }
 
     /**
-     * Modifie la carte <i>Objectif</i> de la colonne.
-     * 
-     * @param carteObjectif La carte <i>Objectif</i> à assigner à la colonne.
-     * 
-     * @since 1.0
-     */
+    * @generated
+    */
     public void setCarteObjectif(CarteObjectif carteObjectif) {
         this.carteObjectif = carteObjectif;
     }
 
-    /**
-     * Ajoute une carte <i>Influence</i> dans la colonne et place celle d'avant en face visible s'il en existe une.
-     * 
-     * @param carte Carte à ajouter dans la colonne.
-     * 
-     * @since 1.0
-     */
     public void addCarteInfluence(CarteInfluence carte) {
-    	for(CarteInfluence c : cartesInfluences) {
-    		if(c == null) {
-    			c = carte;
+    	for(int i = 0; i<cartesInfluences.length; i++) {
+    		if(cartesInfluences[i] == null) {
+    			cartesInfluences[i] = carte;
+    			break;
     		}
     		else {
-    			c.setEstVisible(true);
+    			cartesInfluences[i].setEstVisible(true);
     		}
     	}
     }
     
-    /**
-     * Place la carte <i>Influence</i> spécifiée en face visible.
-     * 
-     * @param carteInfluence La carte <i>Influence</i> à placer en face visible.
-     * 
-     * @since 1.0
-     */
+    //methode qui retourne une carte
     public void tournerCarte(CarteInfluence carteInfluence){
     	carteInfluence.setEstVisible(true);
     }
 
-    /**
-     * Retourne true tant que la colonne n'est pas pleine, false si pleine. Il est important de noter que la taille est calculée
-     * à partir du nombre de joueurs et que la taille maximale d'une colonne est de dix-huit.
-     * 
-     * @return boolean Vrai si la colonne est pleine, faux si elle ne l'est pas.
-     * 
-     * @since 1.0
-     */
+    //methode renvoyant true tant que la colonne n'est pas pleine
+    //renvoie false si pleine (est pleine si �gale � 6+(nb joueurs)*2)
     public boolean estPleine() {
         for (int i = 0; i < cartesInfluences.length; i++)
         {
@@ -119,11 +68,6 @@ public class Colonne {
         return (true);
     }
 
-    /**
-     * Vide la colonne de toutes ses cartes <i>Influence</i>.
-     * 
-     * @since 1.0
-     */
     public void vider() {
         for (int i = 0; i < cartesInfluences.length; i++)
         {
