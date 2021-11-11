9e2095231f169eb79219dcbd30ebdeae1402c9e7
Benjamin Neuville <b.neuville@outlook.fr>
2021-11-11 22:45:43 +0100

    Update Lanceur.java

--- Dossier-General/Programme-Principal/src/main/Lanceur.java
+++ Dossier-General/Programme-Principal/src/main/Lanceur.java
@@ -11,20 +11,17 @@ public class Lanceur {
 	public static void main(String[] args) {
 		//Voir projet de TD d'IHM
 		//Ici on devra lancer lancer le processus (Créer une var sys jsp si vous avez créé une classe "Système")
-		//Joueur you = new Joueur();
-		//Data data = new Data();
-		//data.setJoueurs(null);
-		//data.setPlateau(null);
 		GestionnaireInterface.lancement(args); // lance le programme depuis le gestionnaire d'interface
-//
+
 //		Joueur joueur = new Joueur(Color.AQUA, "test");
 //		Data data = new Data(joueur, 2);
 //		for(int i=0; i<25; i++) {
 //			System.out.println("-----"+data.getJoueurs()[0].getReserve()[i]);
 //		}
-		
-		//Partie partie = new Partie(you, data);
-		//partie.createPartie(data, you);
+//		
+//		System.out.println("--"+data.getJoueurs()[0].getMain()[0]);
+//		System.out.println("--"+data.getJoueurs()[0].getMain()[1]);
+//		System.out.println("--"+data.getJoueurs()[0].getMain()[2]);
 
 	}
 }
