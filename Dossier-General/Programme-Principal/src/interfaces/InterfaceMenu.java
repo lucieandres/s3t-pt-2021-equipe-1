020643f1acd9247138b4a76c5bda9254a608fc25
Lefumeux Bastien <bastien.lefumeux@etu.unice.fr>
2021-11-11 13:52:35 +0100

    MAJ des imports

--- Dossier-General/Programme-Principal/src/interfaces/InterfaceMenu.java
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceMenu.java
@@ -1,23 +1,14 @@
 package interfaces;
 
-import java.awt.BorderLayout;
-
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
-import joueur.Joueur;
 
 /**
 * @generated
