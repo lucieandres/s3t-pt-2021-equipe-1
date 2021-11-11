b03ba3c3614ad04791fbcacb7cb14c5edea63e3b
lucieandres <lucie.andres@etu.unice.fr>
2021-11-11 12:57:23 +0100

    Update ElementPlateau.java

--- Dossier-General/Programme-Principal/src/elements/ElementPlateau.java
+++ Dossier-General/Programme-Principal/src/elements/ElementPlateau.java
@@ -1,59 +1,59 @@
-package elements;
-
-import cartes.CarteInfluence;
-
-import java.util.ArrayList;
-
-import joueur.Joueur;
-
-
-/**
-* @generated
-*/
-public class ElementPlateau {
-    
-    /**
-    * @generated
-    */
-    private ArrayList<CarteInfluence> cartesInfluences;
-    
-    /**
-    * @generated
-    */
-    private Joueur joueur;
-    
-    
-    /**
-    * @generated
-    */
-    public ArrayList<CarteInfluence> getCartesInfluences() {
-        return this.cartesInfluences;
-    }
-    
-    /**
-    * @generated
-    */
-    public void setCartesInfluences(ArrayList<CarteInfluence> cartesInfluences) {
-        this.cartesInfluences = cartesInfluences;
-    }
-    
-    public void addCartesInfluences(CarteInfluence carteInfluence) {
-    	this.cartesInfluences.add(carteInfluence);
-    }
-    
-    /**
-    * @generated
-    */
-    public Joueur getJoueur() {
-        return this.joueur;
-    }
-    
-    /**
-    * @generated
-    */
-    public void setJoueur(Joueur joueur) {
-        this.joueur = joueur;
-    }
-    
-    
-}
+//package elements;
+//
+//import cartes.CarteInfluence;
+//
+//import java.util.ArrayList;
+//
+//import joueur.Joueur;
+//
+//
+///**
+//* @generated
+//*/
+//public class ElementPlateau {
+//    
+//    /**
+//    * @generated
+//    */
+//    private ArrayList<CarteInfluence> cartesInfluences;
+//    
+//    /**
+//    * @generated
+//    */
+//    private Joueur joueur;
+//    
+//    
+//    /**
+//    * @generated
+//    */
+//    public ArrayList<CarteInfluence> getCartesInfluences() {
+//        return this.cartesInfluences;
+//    }
+//    
+//    /**
+//    * @generated
+//    */
+//    public void setCartesInfluences(ArrayList<CarteInfluence> cartesInfluences) {
+//        this.cartesInfluences = cartesInfluences;
+//    }
+//    
+//    public void addCartesInfluences(CarteInfluence carteInfluence) {
+//    	this.cartesInfluences.add(carteInfluence);
+//    }
+//    
+//    /**
+//    * @generated
+//    */
+//    public Joueur getJoueur() {
+//        return this.joueur;
+//    }
+//    
+//    /**
+//    * @generated
+//    */
+//    public void setJoueur(Joueur joueur) {
+//        this.joueur = joueur;
+//    }
+//    
+//    
+//}
