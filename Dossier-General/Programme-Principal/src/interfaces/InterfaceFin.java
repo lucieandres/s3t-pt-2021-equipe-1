697bfe4c9ee542fc462635eef8721b99f1f84f18
Julien <julien.brandi@etu.univ.fr>
2021-11-24 13:59:57 +0100

    InterfaceFin

---
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceFin.java
@@ -0,0 +1,64 @@
+package interfaces;
+
+import java.util.ArrayList;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import joueur.Joueur;
+import moteur.Data;
+
+/**
+ * Cette classe permet de créer une partie.
+ * C'est sur cette interface qu'on peut choisir le nombre de joueurs, rentrer son pseudo et lancer la partie.
+ * 
+ * @author S3T - G1
+ * 
+ * @since 1.0
+ */
+
+public class InterfaceFin extends BorderPane implements UI {
+	
+	public GestionnaireInterface GI = null; // link to the prime instance of GestionnaireInterface is required to go back
+	
+	Label joueur;
+	Button boutonRetour;
+	TextField pseudo;
+	
+	/**
+     *  Ce constructeur permet de créer tous les éléments de l'interface, c'est-à-dire le titre
+     *  la glissière, la zone d'insertion de texte, le bouton retour et le bouton pour lancer la partie.
+     * 
+     * @param gi Le gestionnaire d'interface permettra de relier cette interface aux autres pour qu'elle puisse communiquer ensemble.
+     * 
+     * @since 1.0
+     */
+	public InterfaceFin(GestionnaireInterface gi){
+		super();
+		Data data =  GI.getData();
+		
+		ArrayList<Joueur> joueurs = new ArrayList<Joueur>();
+		
+		joueurs.add(data.getMaster());
+		for(int i = 0; i < data.getJoueurs().length;i++)
+			joueurs.add(data.getJoueurs(i));
+		
+	}
+}
