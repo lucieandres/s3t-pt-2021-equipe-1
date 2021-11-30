7aa5f45ed3858326e2d3a12ff9e2460ec8d198f6
MichelGulasin-Triay <70748262+MichelGulasin-Triay@users.noreply.github.com>
2021-11-30 17:54:22 +0100

    update cartes

--- Dossier-General/Programme-Principal/src/cartes/Marchant.java
+++ Dossier-General/Programme-Principal/src/cartes/Marchant.java
@@ -9,17 +9,33 @@ import javafx.scene.paint.Color;
  * 
  * @since 1.0
  */
-public class Marchant extends CarteDouble{
-	
-    /**
-     * Ce constructeur produit une carte <i>Influence</i> Marchant de la couleur passée en paramètre.
-     * 
-     * @param couleur Couleur de la carte.
-     * 
-     * @since 1.0
-     */
+public class Marchant extends CarteDouble {
+
+	/**
+	 * Ce constructeur produit une carte <i>Influence</i> Marchant de la couleur passée en paramètre.
+	 * 
+	 * @param couleur Couleur de la carte.
+	 * 
+	 * @since 1.0
+	 */
 	public Marchant(Color couleur) {
 		super(couleur, "Marchant", "Commerce");
+	}
+
+	/**
+	 * Retourne la vraie valeur de la carte <i>Influence</i> entre sa valeur (8) et sa valeur spéciale (12).
+	 * 
+	 * @param domaine Le domaine de la carte <i>Objectif</i> de la colonne ou est
+	 *                placée la carte <i>Influence</i>.
+	 * 
+	 * @since 1.0
+	 */
+	public double valeurUtilisee(String domaine) {
+		if (domaine.equals("Commerce")) {
+			return 12;
+		} else {
+			return 8;
+		}
 	}
 
 }
