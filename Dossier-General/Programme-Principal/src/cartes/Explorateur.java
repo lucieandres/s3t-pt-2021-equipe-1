4f688355a17e746eebbce8998029db25ff601ca8
HugoLanzafame <hugo.lanzafame@etu.univ-cotedazur.fr>
2021-12-03 12:12:32 +0100

    Update Explorateur.java
    ajout capacité spéciale (pas fini)

--- Dossier-General/Programme-Principal/src/cartes/Explorateur.java
+++ Dossier-General/Programme-Principal/src/cartes/Explorateur.java
@@ -1,42 +1,63 @@
-//package cartes;
-//
-//import javafx.scene.paint.Color;
-//
-//public class Explorateur extends CarteSpeciale{
-//	
-//	public Explorateur(Color couleur) {
-//		super(couleur, "L'Explorateur", 13);
-//	}
-//
+package cartes;
+
+import elements.Colonne;
+import elements.Plateau;
+import javafx.scene.paint.Color;
+import moteur.Data;
+
+/**
+ * Cette classe définit les cartes <i>Influence</i> spéciale Explorateur dont la valeur est treize.
+ * 
+ * @author S3T - G1
+ * 
+ * @since 1.0
+ */
+public class Explorateur extends CarteSpeciale{
+	
+	/**
+	 * Ce constructeur produit une carte <i>Influence</i> spéciale Explorateur de la couleur passée en paramètre.
+	 * 
+	 * @param couleur Couleur de la carte.
+	 * 
+	 * @since 1.0
+	 */
+	public Explorateur(Color couleur) {
+		super(couleur, "L'Explorateur", 13);
+	}
+
+	
+	/**
+	 * Active la capacité spéciale de la carte Explorateur.
+	 * 
+	 * @param plateau plateau du jeu.
+	 * @throws Exception 
+	 * 
+	 * @since 1.0
+	 */
 //	@Override
-//	public void Activer() {
-//		int numeroColonne = 0;
-//		int numeroColonneVisee;
-//		int nbObjectif = this.getColonne().getPlateau().getColonne().size(); 
-//		
-//		Tempete tempete = new Tempete();
-//		
-//		for(int i=0; i<nbObjectif; i++){
-//			if(this.getColonne().getPlateau().getColonne().get(i) == this.getColonne()){
-//				numeroColonne = i;
-//				break;
-//				}
-//			//TODO erreur si il trouve pas
-//		}
-//		
-//		if(numeroColonne == nbObjectif-1) {
-//			numeroColonneVisee = 0;
-//		}
-//		else {
-//			numeroColonneVisee = numeroColonne+1;
-//		}
-//		
-//		while(!this.getColonne().getPlateau().getColonne().get(numeroColonneVisee).getCartesInfluences().contains(tempete)) {
-//			numeroColonneVisee+=1;
-//		}
-//		
-//		this.Placer(this.getColonne().getPlateau().getColonne().get(numeroColonneVisee));
-//		this.getColonne().getCartesInfluences().remove(this);
-//	}
-//	
-//}
+	public void Activer(Data data) throws Exception {
+		int indexColonneActuelle = data.getPlateau().getIndexColonneCarte(this);
+		int indexColonneVisee = indexColonneActuelle+1;
+		int nbColonne = data.getPlateau().getColonnes().length; 
+		
+		for (int i = 0; i <= data.getPlateau().getColonnes().length; i++) {
+			for(int j=0; j <= data.getPlateau().getColonne(indexColonneVisee).getCartesInfluences().length; j++) {
+				if(data.getPlateau().getColonne(indexColonneVisee).getCarteInfluence(i).getNom()=="Tempete") {
+					indexColonneVisee +=1;
+				}
+				else {
+					break;
+				}
+			}
+
+		indexColonneVisee=indexColonneVisee%nbColonne;
+			
+		data.getPlateau().getColonne(indexColonneVisee).ajouterCarteInfluence(this);
+		data.getPlateau().getColonne(indexColonneActuelle).enleverCarteInfluence(data.getPlateau().getColonne(indexColonneActuelle).getCartesInfluences().length-1);
+		data.getPlateau().getColonne(indexColonneVisee).ajouterCarteInfluence(data.getPlateau().getColonne(indexColonneActuelle).getCarteInfluence(data.getPlateau().getColonne(indexColonneActuelle).getCartesInfluences().length));
+		data.getPlateau().getColonne(indexColonneActuelle).enleverCarteInfluence(data.getPlateau().getColonne(indexColonneActuelle).getCartesInfluences().length);
+		
+		}
+	}
+	
+}
