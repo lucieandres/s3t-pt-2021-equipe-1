923d398d148d7ea2ab3de2403d622870a07ff43d
HugoLanzafame <hugo.lanzafame@etu.univ-cotedazur.fr>
2021-12-03 12:00:52 +0100

    Update CarteSpeciale.java
    Javadoc + modif Activer (param data)

--- Dossier-General/Programme-Principal/src/cartes/CarteSpeciale.java
+++ Dossier-General/Programme-Principal/src/cartes/CarteSpeciale.java
@@ -5,26 +5,51 @@ import javafx.scene.paint.Color;
 
 public abstract class CarteSpeciale extends CarteInfluence {
 	
+    /**
+     * Ce constructeur produit une carte <i>Influence</i> spéciale en lui assignant sa couleur, son nom, sa valeur, sa visibilité avec les variables qui ont été spécifiées.
+     * 
+     * @param couleur La couleur de la carte <i>Influence</i>.
+     * 
+     * @param nom Le nom de la carte <i>Influence</i>.
+     * 
+     * @param valeur La valeur de la carte <i>Influence</i>.
+     * 
+     * @param estVisible Un booléen vrai si la carte <i>Influence</i> est face visible, faux si elle est face cachée.
+     * 
+     * @since 1.0
+     */
 	//Constructeur basique
 	protected CarteSpeciale(Color couleur, String nom, double valeur, boolean estVisible) {
 		super(couleur, nom, valeur, estVisible);
 	}
 	
-	//Constructeur simplifi�
+    /**
+     * Ce constructeur produit une carte <i>Influence</i> spéciale en lui assignant sa couleur, son nom ainsi que sa valeur avec les variables qui ont été spécifiées.
+     * False sera assigné à sa visibilité par défaut, ce qui correspond à face cachée.
+     * 
+     * @param couleur La couleur de la carte <i>Influence</i>.
+     * 
+     * @param nom Le nom de la carte <i>Influence</i>.
+     * 
+     * @param valeur  La valeur de la carte <i>Influence</i>.
+     * 
+     * @since 1.0
+     */
+	//Constructeur simplifié
 	protected CarteSpeciale(Color couleur, String nom, double valeur) {
 		super(couleur, nom, valeur, false);
 	}
 	
 	//Operations
 	
-	public abstract void Activer(Plateau plateau) throws Exception;
-	 
-	// A revoir
-//	public void Reveler() {
-//		this.setEstVisible(true);
-//		if(!this.getColonne().getCarteObjectif().getEstRealise()) {
-//			  this.Activer();
-//		}
-//	}
+    /**
+     * Activation de la capacité spéciale.
+     * 
+     * @param data Les données de la partie.
+     * 
+     * @since 1.0
+     */
+	//Const
+	public abstract void Activer(Data data) throws Exception;
 	
 }
