b4fc07bb6850ef64552019341f3e91418cf93d44
AlexC-univ <90330972+AlexC-univ@users.noreply.github.com>
2021-12-06 17:30:11 +0100

    classe displayColonne

--- Dossier-General/Programme-Principal/src/javaFX_testZone/DisplayColonne.java
+++ Dossier-General/Programme-Principal/src/javaFX_testZone/DisplayColonne.java
@@ -1,7 +1,27 @@
 package javaFX_testZone;
 
+import java.util.HashMap;
+
 import javafx.scene.layout.Pane;
 
 public class DisplayColonne extends Pane {
 	
+	double posY = this.getTranslateY();
+	
+	int maxsize = 0;
+	HashMap<Double,SpriteCarteInfluence> ListeCarte = new HashMap<Double,SpriteCarteInfluence>();
+	SpriteCarteObjectif CarteObjectif = null;
+	
+	public DisplayColonne(SpriteCarteInfluence[] SCI, SpriteCarteObjectif SCO, int height) {
+		maxsize = height;
+		CarteObjectif = SCO;
+		int i=0;
+		for(SpriteCarteInfluence S : SCI) {
+			ListeCarte.put((double) i*S.getHeight(), S);
+			i++;
+		}
+		
+		
+		
+	}
 }
