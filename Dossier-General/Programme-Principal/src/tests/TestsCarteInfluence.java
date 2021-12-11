15e10e34e4f0e979d5bee10bd67635c5d09eab01
lucieandres <lucie.andres@etu.unice.fr>
2021-12-11 18:57:56 +0100

    Update TestsCarteInfluence.java

--- Dossier-General/Programme-Principal/src/tests/TestsCarteInfluence.java
+++ Dossier-General/Programme-Principal/src/tests/TestsCarteInfluence.java
@@ -14,7 +14,7 @@ import cartes.Roi;
 import javafx.scene.paint.Color;
 
 
-class Tests {
+class TestsCarteInfluence {
 
 	@BeforeEach
 	void setUp() throws Exception {
@@ -32,39 +32,45 @@ class Tests {
 		Juliette julBl = new Juliette(Color.WHITE);
 		assertEquals(julBl.getNom(), "Juliette");
 		assertEquals(julBl.getCouleur(), Color.WHITE);
-		assertFalse(julBl.getEstVisible());
+		assertTrue(julBl.getEstVisible());
 		//assertEquals(julBl.getValeur(), 14);
 		julBl.setCouleur(Color.BLUE);
 		julBl.setNom("Reine");
 		julBl.setValeur(10);
-		assertTrue(julBl.getEstVisible());
 		julBl.setEstVisible(false);
+		assertFalse(julBl.getEstVisible());
+		julBl.reveler();
+		assertTrue(julBl.getEstVisible());
 		
 		//test de la classe Reine
 		
 		Reine reiBl = new Reine(Color.WHITE);
 		assertEquals(reiBl.getNom(), "Reine");
 		assertEquals(reiBl.getCouleur(), Color.WHITE);
-		assertFalse(reiBl.getEstVisible());
+		assertTrue(reiBl.getEstVisible());
 		//assertEquals(reiBl.getValeur(), 16);
 		reiBl.setCouleur(Color.BLUE);
 		reiBl.setNom("Roi");
 		reiBl.setValeur(10);
-		assertTrue(reiBl.getEstVisible());
 		reiBl.setEstVisible(false);
+		assertFalse(reiBl.getEstVisible());
+		reiBl.reveler();
+		assertTrue(reiBl.getEstVisible());
 		
 		//test de la classe Roi
 		
 		Roi roiBl = new Roi(Color.WHITE);
 		assertEquals(roiBl.getNom(), "Roi");
 		assertEquals(roiBl.getCouleur(), Color.WHITE);
-		assertFalse(roiBl.getEstVisible());
+		assertTrue(roiBl.getEstVisible());
 		//assertEquals(roiBl.getValeur(), 20);
 		roiBl.setCouleur(Color.BLUE);
 		roiBl.setNom("Reine");
 		roiBl.setValeur(10);
-		assertTrue(roiBl.getEstVisible());
 		roiBl.setEstVisible(false);
+		assertFalse(roiBl.getEstVisible());
+		roiBl.reveler();
+		assertTrue(roiBl.getEstVisible());
 		
 		//Toutes les carteInfluence ont ete testees
 		
