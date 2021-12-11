08f2285837a0fcbaef6449f5c432a7fa5452b3f4
HugoLanzafame <70748395+HugoLanzafame@users.noreply.github.com>
2021-12-11 18:34:55 +0100

    Update CarteARetardement.java
    javadoc

--- Dossier-General/Programme-Principal/src/cartes/CarteARetardement.java
+++ Dossier-General/Programme-Principal/src/cartes/CarteARetardement.java
@@ -5,6 +5,17 @@ import javafx.scene.paint.Color;
 
 public abstract class CarteARetardement extends CarteSpeciale{
 
+    /**
+     * Ce constructeur produit une carte <i>Influence</i> à retardement en lui assignant sa couleur, son nom, sa valeur avec les variables qui ont été spécifiées.
+     * 
+     * @param couleur La couleur de la carte <i>Influence</i>.
+     * 
+     * @param nom Le nom de la carte <i>Influence</i>.
+     * 
+     * @param valeur La valeur de la carte <i>Influence</i>.
+     * 
+     * @since 1.0
+     */
 	protected CarteARetardement(Color couleur, String nom, double valeur) {
 		super(couleur, nom, valeur);
 	}
