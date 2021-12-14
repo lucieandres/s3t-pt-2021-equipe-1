4425312f32686e2610a591075e8bc6687c53c11e
ADAM <oumarov.adam@gmail.com>
2021-12-14 20:26:52 +0100

    ajout .sh macOs

--- Dossier-General/lib/lanceur.sh
+++ Dossier-General/lib/lanceur.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 export DISPLAY=localhost:0
-java --module-path "javaFX/lib" --add-modules javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.web --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar lanceur.jar
+java --module-path "javaFXMacOs/lib" --add-modules javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.web --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar lanceur.jar
