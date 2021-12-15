d7cfc4b8f761e36af3327a543d03216ae380f544
AlexC-univ <90330972+AlexC-univ@users.noreply.github.com>
2021-12-15 10:14:35 +0100

    relocalisation resources

--- Dossier-General/Programme-Principal/src/interfaces/InterfaceBase.java
+++ Dossier-General/Programme-Principal/src/interfaces/InterfaceBase.java
@@ -20,6 +20,8 @@ public class InterfaceBase extends BorderPane {
 	protected LinkedHashMap<Labeled,String> texteInterface = new LinkedHashMap<Labeled,String>();
 	
 	
+	
+	
 	public void goToScene(GestionnaireInterface GI,InterfaceBase Destination,Transition TransitionType) {
 		switch(TransitionType) {
 		case FADEBOUNCE:
@@ -66,7 +68,7 @@ public class InterfaceBase extends BorderPane {
 		}
 	}
 	
-	public void rafraichirTexte(HashMap<Labeled,String> texte, Properties fichierTexte) {
+	public void rafraichirTexte(Properties fichierTexte) {
 		for (Entry<Labeled, String> element : texteInterface.entrySet()) {
 			element.getKey().setText(fichierTexte.getProperty(element.getValue()));
         }
