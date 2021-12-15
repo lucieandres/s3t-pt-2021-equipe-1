c9cffe76b3f655cd2c9590b4e7a1bee5b41f1721
krystodagues <krystoddlh@gmail.com>
2021-12-15 20:23:00 +0100

    javadoc lanceur?

--- Dossier-General/Programme-Principal/src/main/Lanceur.java
+++ Dossier-General/Programme-Principal/src/main/Lanceur.java
@@ -4,7 +4,10 @@ import interfaces.GestionnaireInterface;
 import pp.GestionnaireInterfacePP;
 
 public class Lanceur {
-
+/**
+ * La classe qui lance le programme depuis le gestionnaire d'interface.
+ * @param args les arguments lors du lancement.
+ */
 	public static void main(String[] args) {
 		GestionnaireInterface.lancement(args); // lance le programme depuis le gestionnaire d'interface	
 		//GestionnaireInterfacePP.lancement(args); // lance le programme depuis le gestionnaire d'interface	
