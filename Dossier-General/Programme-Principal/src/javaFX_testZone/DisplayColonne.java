696032692cd9c9e8da1f4dae7358a63ecfa01f15
krystodagues <krystoddlh@gmail.com>
2021-12-15 20:10:30 +0100

    javadoc displaycolonne

--- Dossier-General/Programme-Principal/src/javaFX_testZone/DisplayColonne.java
+++ Dossier-General/Programme-Principal/src/javaFX_testZone/DisplayColonne.java
@@ -4,6 +4,11 @@ import java.util.HashMap;
 
 import javafx.scene.layout.Pane;
 
+/**
+ *  Classe qui affiche les colonnes
+ * @author S3T - G1
+ *
+ */
 public class DisplayColonne extends Pane {
 	
 	double posY = this.getTranslateY();
@@ -12,6 +17,15 @@ public class DisplayColonne extends Pane {
 	HashMap<Double,SpriteCarteInfluence> ListeCarte = new HashMap<Double,SpriteCarteInfluence>();
 	SpriteCarteObjectif CarteObjectif = null;
 	
+	/**
+	 *  constructeur de laclasse qui affiche les colonnes
+	 * @author S3T - G1
+	 * 
+	 * @param SCI affichage des cartes influences.
+	 * @param SCO affichage des cartes objectifs.
+	 * @param height hauteur des colonnes.
+	 *
+	 */
 	public DisplayColonne(SpriteCarteInfluence[] SCI, SpriteCarteObjectif SCO, int height) {
 		maxsize = height;
 		CarteObjectif = SCO;
