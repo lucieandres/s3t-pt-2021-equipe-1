87be37b2112a011985ae86f9f679a6d754ead3fd
krystodagues <krystoddlh@gmail.com>
2021-12-15 20:24:33 +0100

    Update Lanceur.java

--- Dossier-General/Programme-Principal/src/main/Lanceur.java
+++ Dossier-General/Programme-Principal/src/main/Lanceur.java
@@ -2,10 +2,14 @@ package main;
 
 import interfaces.GestionnaireInterface;
 import pp.GestionnaireInterfacePP;
-
+/**
+ * Le lanceur du programme.
+ * @author S3T - G1
+ *
+ */
 public class Lanceur {
 /**
- * La classe qui lance le programme depuis le gestionnaire d'interface.
+ * La classe main qui lance le programme depuis le gestionnaire d'interface.
  * @param args les arguments lors du lancement.
  */
 	public static void main(String[] args) {
