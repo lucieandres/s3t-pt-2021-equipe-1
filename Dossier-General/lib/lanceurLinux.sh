9a5af3196c8982ed4b88cc9c40d30bcfbfd3351b
ClementREMY2 <clement.remy@etu.unice.fr>
2021-12-16 23:56:54 +0100

    Reorganisation de l'arborescence pour les executables + MAJ de ceux ci

--- Dossier-General/lib/lanceurLinux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java --module-path "javaFX/Linux/lib" --add-modules javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.web --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar lanceur.jar
---
+++ Dossier-General/lib/lanceurs/Windows/lanceurWindowsJoueur.bat
@@ -0,0 +1,1 @@
+java -p "..\..\javaFX\Windows\lib" --add-modules javafx.controls,javafx.base,javafx.fxml,javafx.graphics,javafx.media,javafx.web --add-opens=javafx.graphics/javafx.scene=ALL-UNNAMED --add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED -jar ..\jar\lanceurJoueur.jar
