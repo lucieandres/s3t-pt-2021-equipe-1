9c3373d9519b22b419db0173b48b5039d2394970
God-Sama <65094816+god-sama@users.noreply.github.com>
2021-12-28 21:24:51 +0100

    [u] PlanDeTestBot.java

--- Dossier-General/Programme-Principal/src/main/PlanDeTestBot.java
+++ Dossier-General/Programme-Principal/src/main/PlanDeTestBot.java
@@ -1,22 +1,19 @@
 package main;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.TreeMap;
 
 import bot.Bot;
 import javafx.scene.paint.Color;
 import joueur.Joueur;
 import moteur.Data;
-import interfaces.GestionnaireInterface;
 
 public class PlanDeTestBot {
 	public static boolean estFinie = false;
 	public static Data data;
-	public static LinkedHashMap<Joueur, Integer> score = new LinkedHashMap<Joueur, Integer>();
-	public static LinkedHashMap<Joueur, Integer> victoire = new LinkedHashMap<Joueur, Integer>();
-	
+	public static LinkedHashMap<Integer, Integer> total_score = new LinkedHashMap<Integer, Integer>();
+	public static LinkedHashMap<Integer, Integer> victoire = new LinkedHashMap<Integer, Integer>();
+
 	public static void main(String[] args) {
         LinkedHashMap<Integer, Color> colorInt = new LinkedHashMap<Integer, Color>();
     	colorInt.put(0, Color.YELLOW);
@@ -25,38 +22,42 @@ public class PlanDeTestBot {
     	colorInt.put(3, Color.DARKGREEN);
     	colorInt.put(4, Color.RED);
     	colorInt.put(5, Color.BLUE);
-    	
-		Joueur[] listeBot = new Joueur[2];
-		
-			listeBot[0] = new Bot("facile", colorInt.get(0), "botf" + 0);
-			listeBot[1] = new Bot("moyen", colorInt.get(5), "botm" + 1);
-		
-		for(Joueur j : listeBot) {
-			score.put(j, 0);
-			victoire.put(j, 0);
+
+
+    	Joueur[] listeBot = new Joueur[2];
+
+		for(int i = 0; i < listeBot.length; i++) {
+			total_score.put(i, 0);
+			victoire.put(i, 0);
 		}
-		
+
 		int nbtest = 100;
 		int currenttest = 0;
-		
+
 		while(nbtest > currenttest) {
+			listeBot[0] = new Bot("facile", colorInt.get(0), "botm" + currenttest);
+			listeBot[1] = new Bot("facile", colorInt.get(5), "botf" + currenttest);
 			data = new Data(listeBot);
+			for(int i = 0; i < data.getJoueurs().length; i++) {
+				System.out.println("after init: " + listeBot[i].getPseudo() + " : " + data.getJoueurs()[i].getScore() + " - " + victoire.get(i));
+			}
+
 			try {
 				doitJouer();
 			} catch (Exception e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+			data.calculScoreJoueurs();
 			stat();
 			currenttest++;
 		}
-		for(int i = 0; i<listeBot.length; i++) {
-			System.out.println(listeBot[i].getPseudo() + " : " + score.get(listeBot[i]) + " - " + victoire.get(listeBot[i]));
+		for(int i = 0; i < listeBot.length; i++) {
+			System.out.println(listeBot[i].getPseudo() + " : " + total_score.get(i) + " - " + victoire.get(i));
 		}
 	}
-	
+
 	public static void doitJouer() throws Exception {
-		if(!verifManche() && estFinie == false) {
+		if(!verifManche() && !estFinie) {
 	    	if(data.getJoueurs()[data.getCurrentJoueur()] instanceof Bot) {
 	    		data.getJoueurs()[data.getCurrentJoueur()].jouer(data, 0, 0);
 	    		doitJouer();
@@ -68,6 +69,7 @@ public class PlanDeTestBot {
 	        	data.activerCartesARetardement();
 	        	data.retournerCarte();
 	        	data.finDeManche();
+	        	data.calculScoreJoueurs();
 	    		estFinie = false;
 	    		doitJouer();
     		}
@@ -75,28 +77,44 @@ public class PlanDeTestBot {
 	    		estFinie = true;
 	        	data.retournerCarte();
 	        	data.finDeManche();
+	        	data.calculScoreJoueurs();
     		}
     	}
 	}
-    
+
     public static boolean verifManche() {
     	boolean verif = true;
-    	for(int i = 0; i<data.getPlateau().getColonnes().length; i++) {
+    	for(int i = 0; i < data.getPlateau().getColonnes().length; i++) {
     		if(!data.getPlateau().getColonnes()[i].estPleine() && !data.getPlateau().getColonnes()[i].estFiniEtreRempli()) {
     			verif = false;
     		}
     	}
     	return verif;
     }
-    
+
     public static void stat() {
-    	HashMap<Integer, Joueur> scores = new HashMap<>();
-		for(Joueur j : data.getJoueurs()) {
-			scores.put(j.getScore(), j);
-			score.replace(j, score.get(j) + j.getScore());
+    	HashMap<Integer, Integer> manche_score = new HashMap<>();
+		for(int i = 0; i < data.getJoueurs().length; i++) {
+			manche_score.put(i, data.getJoueurs()[i].getScore());
+			total_score.replace(i, total_score.get(i) + data.getJoueurs()[i].getScore());
 		}
-		TreeMap<Integer, Joueur> triScores = new TreeMap<>(scores);
-		ArrayList<Joueur> triJoueur = new ArrayList<>(triScores.values());
-		victoire.replace(triJoueur.get(triScores.size()-1), (victoire.get(triJoueur.get(triScores.size()-1))+1));
+		int best = getMax(manche_score);
+		System.out.println(best + " " + manche_score.get(best));
+		victoire.replace(best, victoire.get(best) + 1);
+    }
+
+    public static int getMax(HashMap<Integer, Integer> manche_score)
+    {
+    	int max = 0;
+    	int maxIndex = 0;
+    	for (Integer i : manche_score.keySet())
+    	{
+    		if (manche_score.get(i) > max)
+    		{
+    			max = manche_score.get(i);
+    			maxIndex = i;
+    		}
+    	}
+    	return (maxIndex);
     }
 }
