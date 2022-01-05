bcde89c4aa8b950c158d4ca2f4c3f5ec09a658c7
Julien <julien.brandi@etu.univ.fr>
2022-01-05 13:30:05 +0100

    Update InterfaceLobby.java

--- Dossier-General/Programme-Principal/src/pp/InterfaceLobby.java
+++ Dossier-General/Programme-Principal/src/pp/InterfaceLobby.java
@@ -1,7 +1,9 @@
 package pp;
 
 import javafx.event.Event;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
@@ -21,8 +23,12 @@ public class InterfaceLobby extends InterfaceBase{
 	
 	private VBox VBMilieu;
 	int nbjoueur;
+	int nbBox = 0;
+	int nbBot = 0;
+	GestionnaireInterfacePP gi;
 	
 	public InterfaceLobby(GestionnaireInterfacePP GI){
+		GI = gi;
 	}
 	
 	public void dessineInterface(GestionnaireInterfacePP GI) {
@@ -31,9 +37,10 @@ public class InterfaceLobby extends InterfaceBase{
 		HBox[] HBJoueurs = new HBox[nbjoueur];
 		Button[] ajouterJoueurs = new Button[nbjoueur];
 		
+		
 		for(int i = 0; i < nbjoueur; i++) {
 			ajouterJoueurs[i] = new Button("+");
-			ajouterJoueurs[i].setPrefSize(GI.screenBounds.getHeight()*0.45, GI.screenBounds.getWidth()*0.06);
+			ajouterJoueurs[i].setPrefSize(GI.screenBounds.getHeight()*0.48, GI.screenBounds.getWidth()*0.06);
 			ajouterJoueurs[i].setAlignment(Pos.CENTER);
 		}
 				
@@ -46,79 +53,29 @@ public class InterfaceLobby extends InterfaceBase{
 			final int k = i;
 			ajouterJoueurs[i].setOnAction(e -> dessineBoxBot(GI,HBJoueurs[k]));
 		}
-		
 		this.setCenter(VBMilieu);		
 	}
 	
 	public HBox dessineBoxJoueur(GestionnaireInterfacePP GI) {
 		HBox BoxJoueur = new HBox();
 		BoxJoueur.setAlignment(Pos.CENTER);
-		CheckBox[] couleur = new CheckBox[6];
 		
 		VBox VBBouton = new VBox();
 		Button sup = new Button("^");
-		sup.setPrefSize(15, 15);
+		sup.setFont(Font.font("Comic Sans MS", 10));
+		sup.setPrefSize(18, 30);
+		sup.setOnAction(e -> hausserBox(GI, BoxJoueur));
 		Button inf = new Button("v");
-		inf.setPrefSize(15, 15);
+		inf.setPrefSize(18, 30);
+		inf.setFont(Font.font("Comic Sans MS", 10));
+		inf.setOnAction(e -> descendreBox(GI, BoxJoueur));
 		VBBouton.getChildren().addAll(sup, inf);
 		
 		TextField pseudo = new TextField("Pseudo");
 //		pseudo.setPromptText(GI.texteLangue.get(GI.langueSelectionne).getProperty("texte.pseudo"));
         pseudo.setFont(Font.font("Comic Sans MS", 20));
         pseudo.setPrefSize(220, 50);
-		
-		for(int i=0; i<6; i++) {
-			couleur[i] = new CheckBox();
-			couleur[i].setPrefSize(5, 5);
-		}
-		
-		couleur[0].setStyle(".check-box > .box {-fx-background-color: red;}");
-		couleur[1].setBackground(new Background(new BackgroundFill(Color.web("0x800080ff"), CornerRadii.EMPTY, null)));
-		couleur[2].setBackground(new Background(new BackgroundFill(Color.web("0xfaebd7ff"), CornerRadii.EMPTY, null)));
-		couleur[3].setBackground(new Background(new BackgroundFill(Color.web("0x006400ff"), CornerRadii.EMPTY, null)));
-		couleur[4].setBackground(new Background(new BackgroundFill(Color.web("0xff0000ff"), CornerRadii.EMPTY, null)));
-		couleur[5].setBackground(new Background(new BackgroundFill(Color.web("0x0000ffff"), CornerRadii.EMPTY, null)));
-		
-		GridPane tabCouleur = new GridPane();
-		
-		tabCouleur.add(couleur[0], 0, 0);
-		tabCouleur.add(couleur[1], 1, 0);
-		tabCouleur.add(couleur[2], 2, 0);
-		tabCouleur.add(couleur[3], 0, 1);
-		tabCouleur.add(couleur[4], 1, 1);
-		tabCouleur.add(couleur[5], 2, 1);
-		
-		tabCouleur.setHgap(5);
-		tabCouleur.setVgap(5);
-		
-		BoxJoueur.getChildren().addAll(VBBouton, pseudo, tabCouleur);
-		
-		return BoxJoueur;
-	}
-	
-	public void dessineBoxBot(GestionnaireInterfacePP GI, HBox HBJoueurs) {
-        System.out.println("On appelle bien la fonction");
-        HBox BoxBot = new HBox();
-        BoxBot.setPrefSize(GI.screenBounds.getHeight()*0.45, GI.screenBounds.getWidth()*0.06);
-        BoxBot.setAlignment(Pos.CENTER);
-
-        VBox VBBouton = new VBox();
-        Button sup = new Button("^");
-        sup.setPrefSize(15, 15);
-        Button inf = new Button("v");
-        inf.setPrefSize(15, 15);
-        VBBouton.getChildren().addAll(sup, inf);
-
-        TextField pseudo = new TextField("Pseudo");
-//        pseudo.setPromptText(GI.texteLangue.get(GI.langueSelectionne).getProperty("texte.pseudo"));
-        pseudo.setFont(Font.font("Comic Sans MS", 20));
-        pseudo.setPrefSize(220, 50);
-
-        ChoiceBox<String> difficulteBot = new ChoiceBox<>();
-        difficulteBot.getItems().add("Facile");
-        difficulteBot.getItems().add("Moyen");
-        difficulteBot.getItems().add("Difficile");
-
+        
         ChoiceBox<String> choixCouleur = new ChoiceBox<>();
         String rouge = new String("Rouge");
         String violet = new String("Violet");
@@ -127,9 +84,63 @@ public class InterfaceLobby extends InterfaceBase{
         String vert = new String("Vert");
         String bleu = new String("Bleu");
         choixCouleur.getItems().addAll(rouge, violet, jaune, blanc, vert, bleu);
+        
+		BoxJoueur.getChildren().addAll(VBBouton, pseudo);
+		
+		return BoxJoueur;
+	}
+	
+	public void dessineBoxBot(GestionnaireInterfacePP GI, HBox HBJoueurs) {
+        HBox BoxBot = new HBox();
+        BoxBot.setPrefSize(GI.screenBounds.getHeight()*0.48, GI.screenBounds.getWidth()*0.06);
+        BoxBot.setAlignment(Pos.CENTER_LEFT);
+        nbBot++;
+        
+        VBox VBBouton = new VBox();
+        Button sup = new Button("^");
+        sup.setPrefSize(38, 50);
+        sup.setFont(Font.font("Comic Sans MS", 18));
+        sup.setOnAction(e -> hausserBox(GI, BoxBot));
+        Button inf = new Button("v");
+        inf.setPrefSize(38, 50);
+        inf.setFont(Font.font("Comic Sans MS", 18));
+        inf.setOnAction(e -> descendreBox(GI, BoxBot));
+        VBBouton.getChildren().addAll(sup, inf);
+        VBBouton.setPadding(new Insets(8));
+        
+        TextField pseudo = new TextField();
+        pseudo.setPromptText("bot" + nbBot);
+//        pseudo.setPromptText(GI.texteLangue.get(GI.langueSelectionne).getProperty("texte.pseudo"));
+        pseudo.setFont(Font.font("Comic Sans MS", 20));
+        pseudo.setPrefSize(220, 40);
 
-        BoxBot.getChildren().addAll(VBBouton, pseudo, difficulteBot, choixCouleur);
-
+        ChoiceBox<String> difficulteBot = new ChoiceBox<>();
+        difficulteBot.setPrefSize(100, 40);
+        difficulteBot.getItems().addAll("Facile", "Moyen", "Difficile");
+        difficulteBot.setValue("Facile");
+        
+        ChoiceBox<String> choixCouleur = new ChoiceBox<>();
+        choixCouleur.setPrefSize(100, 40);
+        String rouge = new String("Rouge");
+        String violet = new String("Violet");
+        String jaune = new String("Jaune");
+        String blanc = new String("Blanc");
+        String vert = new String("Vert");
+        String bleu = new String("Bleu");
+        choixCouleur.getItems().addAll(rouge, violet, jaune, blanc, vert, bleu);
+        
+        HBox boxMoins = new HBox();
+        boxMoins.setPadding(new Insets(0,0,15,15));
+        
+        Button retirerBox = new Button("-");
+        retirerBox.setFont(Font.font("Comic Sans MS", 15));
+        retirerBox.setPrefSize(30, 30);
+        retirerBox.setOnAction(e -> retirerBox(GI, BoxBot));
+        
+        boxMoins.getChildren().add(retirerBox);
+        
+        BoxBot.getChildren().addAll(VBBouton, pseudo, difficulteBot, choixCouleur, boxMoins);
+        
         int k = 0;
 
         for(int i = 0; i<nbjoueur; i++)
@@ -137,8 +148,57 @@ public class InterfaceLobby extends InterfaceBase{
                 k = i;
 
         VBMilieu.getChildren().remove(HBJoueurs);
-        VBMilieu.getChildren().add(k, BoxBot);
-        System.out.println("On finit la fonction");
+        VBMilieu.getChildren().add(nbBox, BoxBot);
+        nbBox++;
     }
+	
+	public void retirerBox(GestionnaireInterfacePP GI, HBox Box) {
+		VBMilieu.getChildren().remove(Box);
+		nbBox--;
+		if(Box.getChildren().size() == 5)
+			nbBot--;
+			
+		HBox HBPlus = new HBox();
+		Button ajouterJoueur = new Button("+");
+		ajouterJoueur.setPrefSize(GI.screenBounds.getHeight()*0.48, GI.screenBounds.getWidth()*0.06);
+		ajouterJoueur.setAlignment(Pos.CENTER);
+		ajouterJoueur.setOnAction(e -> dessineBoxBot(GI, HBPlus));
 
+		HBPlus.getChildren().add(ajouterJoueur);
+		
+		VBMilieu.getChildren().add(HBPlus);
+		
+	}
+	
+	public void descendreBox(GestionnaireInterfacePP GI, HBox Box) {
+		int indexBoxAMonter = VBMilieu.getChildren().indexOf(Box) + 1;
+		int indexBoxADescendre = VBMilieu.getChildren().indexOf(Box);
+		
+		if(indexBoxAMonter > nbjoueur) {
+			
+		}
+		else {
+			Node HBbas = VBMilieu.getChildren().get(indexBoxAMonter);
+			VBMilieu.getChildren().removeAll(HBbas, Box);
+			VBMilieu.getChildren().add(indexBoxADescendre, HBbas);
+			VBMilieu.getChildren().add(indexBoxAMonter, Box);
+		}
+			
+	}
+	
+	public void hausserBox(GestionnaireInterfacePP GI, HBox Box) {
+		int indexBoxAMonter = VBMilieu.getChildren().indexOf(Box);
+		int indexBoxADescendre = VBMilieu.getChildren().indexOf(Box) - 1;
+		
+		if(indexBoxADescendre < 0) {
+			
+		}
+		else {
+			Node HBhaut = VBMilieu.getChildren().get(indexBoxADescendre);
+			VBMilieu.getChildren().removeAll(HBhaut, Box);
+			VBMilieu.getChildren().add(indexBoxADescendre, Box);
+			VBMilieu.getChildren().add(indexBoxAMonter, HBhaut);
+		}
+			
+	}
 }
